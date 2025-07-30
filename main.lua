dc4371c241e3d755a884f3e3349c52b976f3e513
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-30 18:02:20 -0500

    hello world

--- main.lua
+++ main.lua
@@ -1,8 +1,13 @@
 function love.load()
+        
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+        love.graphics.setColor(0,255,0,255)
+        love.graphics.rectangle('fill', 0, 0, 1280, 720)
+        love.graphics.setColor(255,255,255,255)
+        love.graphics.print('Hello World!' 400, 300)
 end
