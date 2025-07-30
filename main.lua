92b5b55005547a445bb6f6b10787f312b5c91e81
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-30 18:06:31 -0500

    moving ball with delta time

--- main.lua
+++ main.lua
@@ -1,13 +1,36 @@
 function love.load()
-        
+        player = {}
+
+        player.speed = 4
+
+        player.x = 400
+        player.y = 200
 end
 
 function love.update(dt)
+
+        if love.keyboard.isDown('right') then
+                player.x = player.x + player.speed * dt
+        end
+
+        if love.keyboard.isDown('left') then
+                player.x = player.x - player.speed * dt
+        end
+
+        if love.keyboard.isDown('down') then
+                player.y = player.y + player.speed * dt
+        end
+
+        if love.keyboard.isDown('up') then
+                player.y = player.y - player.speed * dt
+        end
+
 end
 
 function love.draw()
         love.graphics.setColor(0,255,0,255)
         love.graphics.rectangle('fill', 0, 0, 1280, 720)
+
         love.graphics.setColor(255,255,255,255)
-        love.graphics.print('Hello World!' 400, 300)
+        love.graphics.circle('fill', player.x, player.y, 100)
 end
